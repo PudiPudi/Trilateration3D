533f9c4a8a76190ae6f1b7546ebc9a423ef8f521
Unknown <ludovic.marchand@hotmail.be>
2018-02-13 19:56:46 +0100

    Tests Trialteration 3D

--- Example/Tests/Trilateration3DTests.swift
+++ Example/Tests/Trilateration3DTests.swift
@@ -44,7 +44,7 @@ class Trialteration3DTests: QuickSpec {
         }
         context("Trialterate") {
             
-            it("can Trialterate 2D") {
+            it("can Trilaterate 2D") {
                 let v1 = Vector3(x: 1, y: 1, z: 0)
                 let v2 = Vector3(x: 3, y: 1, z: 0)
                 let v3 = Vector3(x: 3, y: 3, z: 0)
@@ -66,16 +66,34 @@ class Trialteration3DTests: QuickSpec {
                     }
                 }
                 else {
-                    fail("Retunred \"nil\"")
+                    fail("Returned \"nil\"")
                 }
             }
             
-            it("can create custom Position") {
-                let vector = Vector3(x: 2.1, y: 2.2, z: 2.3)
-                let pos = Position(location: vector, range: 23.3, name: "Test")
-                expect(pos.loc.x) == vector.x
-                expect(pos.range) == 23.3
-                expect(pos.name) == "Test"
+            it("can Trialterate 3D") {
+                let v1 = Vector3(x: 0, y: 0, z: 0)
+                let v2 = Vector3(x: 4, y: 0, z: 0)
+                let v3 = Vector3(x: 4, y: 4, z: 4)
+                //-
+                let pos1 = Position(location: v1, range: 3.3)
+                let pos2 = Position(location: v2, range: 3.3)
+                let pos3 = Position(location: v3, range: 3.3)
+                //-
+                if let results = trilaterate(p1: pos1, p2: pos2, p3: pos3, returnMiddle: true) {
+                    
+                    if results.count > 1 {
+                        fail("Only one solution expected")
+                    }
+                    else {
+                        let r = results.first!
+                        expect(r.x) == Float(2)
+                        expect(r.y) == Float(2)
+                        expect(r.z) == Float(2)
+                    }
+                }
+                else {
+                    fail("Returned \"nil\"")
+                }
             }
         }
     }
